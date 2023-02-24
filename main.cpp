e3ed03bca153f2a97635ed2a77899eec59723a44
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2023-02-23 19:09:55 -0500

    included all function test calls

--- main.cpp
+++ main.cpp
@@ -1,1 +1,56 @@
 
+#include <iostream>
+#include "funcs.h"
+#include <string>
+
+int main ()
+{
+  //Task A Tests
+  std::cout << box(3,5);
+  std::cout << "\n------------------\n";
+  std::cout << box(14,26);// seperator 
+  std::cout << "\n------------------\n";
+
+  
+  //Task B Tests
+  std::cout << checkerboard(11,6);
+  std::cout << "\n------------------\n";
+  std::cout << checkerboard(20,20);
+  std::cout << "\n------------------\n";
+
+  
+
+  //Task C Tests
+
+  std::cout << cross(8);
+  std::cout << "\n------------------\n";
+  std::cout << cross(11);
+  std::cout << "\n------------------\n";
+  
+
+  
+  //Task D Tests
+  std::cout << lower(6);
+  std::cout << "\n------------------\n";
+  std::cout << lower(10);
+  std::cout << "\n------------------\n";
+
+  
+  //Task E Tests 
+  std::cout << upper(5);
+  std::cout << "\n------------------\n";
+  std::cout << upper(14);
+  std::cout << "\n------------------\n";
+
+  
+  //Task F Tests 
+  std::cout << trapezoid(12,5);
+  std::cout << "\n------------------\n";
+  std::cout << trapezoid(12,7);
+  std::cout << "\n------------------\n";
+
+  
+  //Task G Tests
+  std::cout << checkerboard3x3(27,27);
+  std::cout << "\n------------------\n";
+  
